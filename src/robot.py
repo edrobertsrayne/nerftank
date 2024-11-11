8fa0cfd20d644f36fcb4b7c23502e2601b5485ba
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-11-11 20:52:18 +0000

    refactor(robot): add docstrings and types

--- src/robot.py
+++ src/robot.py
@@ -2,15 +2,39 @@ from tb6612fng import Motor
 
 
 class Robot:
-    def __init__(self):
-        self.left_motors = [Motor(14, 12, 13), Motor(15, 16, 17)]
-        self.right_motors = [Motor(14, 12, 13), Motor(15, 16, 17)]
+    def __init__(self, left_motor_pins: list, right_motor_pins: list):
+        """
+        Initialize the robot with left and right motor pins.
+
+        :param left_motor_pins: List of pin numbers for the left motors
+        :param right_motor_pins: List of pin numbers for the right motors
+        """
+        self.left_motors = [Motor(pin[0], pin[1], pin[2]) for pin in left_motor_pins]
+        self.right_motors = [Motor(pin[0], pin[1], pin[2]) for pin in right_motor_pins]
 
     @staticmethod
-    def _map(x, in_min, in_max, out_min, out_max):
+    def _map(
+        x: float, in_min: float, in_max: float, out_min: float, out_max: float
+    ) -> float:
+        """
+        Map a value from one range to another.
+
+        :param x: Value to map
+        :param in_min: Minimum value of the input range
+        :param in_max: Maximum value of the input range
+        :param out_min: Minimum value of the output range
+        :param out_max: Maximum value of the output range
+        :return: Mapped value
+        """
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
-    def drive(self, speed, turn):
+    def drive(self, speed: float, turn: float):
+        """
+        Drive the robot.
+
+        :param speed: Speed of the robot (-100 to 100)
+        :param turn: Turn of the robot (-100 to 100)
+        """
         speed = self._map(speed, -100, 100, -1023, 1023)
         turn = self._map(turn, -100, 100, -511, 511)
 
