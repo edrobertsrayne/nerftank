867aaa1d6a9e9e47faac04f799b5cbb8e650eda2
Ed Roberts Rayne <ed.rayne@gmail.com>
2024-11-17 22:29:28 +0000

    refactor: move Robot.map to utils.remap

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+def remap(
+    x: float, in_min: float, in_max: float, out_min: float, out_max: float
+) -> float:
+    """
+    Map a value from one range to another.
+
+    :param x: Value to map
+    :param in_min: Minimum value of the input range
+    :param in_max: Maximum value of the input range
+    :param out_min: Minimum value of the output range
+    :param out_max: Maximum value of the output range
+    :return: Mapped value
+    """
+    return float(x - in_min) * (out_max - out_min) / float(in_max - in_min) + out_min
